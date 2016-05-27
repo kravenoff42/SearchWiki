3a286a6c703afe3a68f709af3ce2af02f95d19fc
kravenoff42 <jcraven42@gmail.com>
2016-05-27 00:05:54 -0400

    imported from CodePen

---
+++ script.js
@@ -0,0 +1,21 @@
+$(document).ready( function()
+{
+	$('form').submit(function(event)
+  {
+		var $input = $(event.target).find('input');
+		var search = $input.val();
+  	if (search != "")
+   {
+    $.getJSON("https://en.wikipedia.org/w/api.php?action=query&format=json&list=search&titles=Main+Page&srsearch="+search+"&srprop=wordcount%7Ctimestamp%7Csnippet?callback=?",
+    function(data)
+    {
+    var item = data.query.search[3].title;
+    var itemDesc = data.query.search[3].snippet;
+    var html = $('<li>').html("<h3>"+item+"</h3>\"..."+itemDesc+"...\"");
+		$("html").appendTo("#list");
+    });
+   }
+
+	});
+
+});
