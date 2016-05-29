6b81ea3082250b77f53808db10ebb761d1e7a8b2
kravenoff42 <jcraven42@gmail.com>
2016-05-29 13:33:06 -0400

    added Jade and Sass

--- Output/script.js
+++ Output/script.js
@@ -6,13 +6,13 @@ $(document).ready( function()
 		var search = $input.val();
   	if (search != "")
    {
-    $.getJSON("https://en.wikipedia.org/w/api.php?action=query&format=json&list=search&titles=Main+Page&srsearch="+search+"&srprop=wordcount%7Ctimestamp%7Csnippet?callback=?",
+    $.getJSON("https://en.wikipedia.org/w/api.php?action=query&format=json&list=search&titles=Main+Page&srsearch="+search+"&srprop=wordcount%7Ctimestamp%7Csnippet",
     function(data)
     {
     var item = data.query.search[3].title;
     var itemDesc = data.query.search[3].snippet;
     var html = $('<li>').html("<h3>"+item+"</h3>\"..."+itemDesc+"...\"");
-		$("html").appendTo("#list");
+		$(html).appendTo("#list");
     });
    }
 
